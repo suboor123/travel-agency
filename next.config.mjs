856581c024379a3d57e7585c52467a80b621a338
Suboor Khan <suboorkhan@krapton.com>
2024-11-03 20:32:53 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                // Match any route and rewrite to the static HTML file
+                source: '/',
+                destination: '/index.html',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
